db3dec05d3f20859d820d1d3e7a63da9ff09d723
Alvaro Cabrera <aduran@expansion.com.mx>
2013-10-29 16:02:29 -0600

    Adds JSON reporter (?)

---
+++ lib/Spectre/Report/JSON.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Spectre\Report;
+
+class JSON
+{
+  public $status = -1;
+
+  public function __construct(array $report)
+  {
+    $this->spec = $report;
+  }
+
+  public function __toString()
+  {
+    list($err, $all) = $this->report($this->spec);
+
+    $this->status = $err;
+
+    return $this->format(array(
+      'tests' => $all,
+      'errors' => $err,
+      'success' => $all - $err,
+      'results' => $this->spec,
+    )) . "\n";
+  }
+
+  private function report(array $set)
+  {
+    $tests = 0;
+    $errors = 0;
+
+    foreach ($set['groups'] as $label => $sub) {
+      if (!empty($sub['results'])) {
+        foreach ($sub['results'] as $subject => $result) {
+          $tests++;
+          $errors += $result[0];
+        }
+      }
+
+      if (!empty($sub['groups'])) {
+        list($err, $all) = $this->report($sub);
+
+        $tests += $all;
+        $errors += $err;
+      }
+    }
+
+    return array($errors, $tests);
+  }
+
+  private function format($data, $depth = 1)
+  {
+    $out = array();
+    $tabs = str_repeat('  ', $depth);
+    $assoc = is_object($data) || !is_numeric(key($data));
+
+    foreach ($data as $key => $val) {
+      $key = json_encode($key, JSON_NUMERIC_CHECK);
+
+      if (is_object($val) || is_array($val)) {
+        $val = $this->format($val, $depth + 1);
+      } else {
+        $val = json_encode($val);
+      }
+
+      $out []= $assoc ? "$tabs$key: $val" : $val;
+    }
+
+    if ($assoc) {
+      return "{\n" . join(",\n", $out) . '}';
+    } else {
+      return '[' . join(',', $out) . ']';
+    }
+  }
+}
