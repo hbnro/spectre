26b0eb33cf1917cd81ad489a336e09ebc2264326
Alvaro Cabrera <aduran@expansion.com.mx>
2013-10-29 22:43:38 -0600

    Do not check types; fix

--- lib/Spectre/Matchers/ToEqual.php
+++ lib/Spectre/Matchers/ToEqual.php
@@ -6,6 +6,6 @@ class ToEqual extends Base
 {
   public function execute($value)
   {
-    return (is_scalar($this->expected) && is_scalar($value)) && ($this->expected == $value);
+    return $this->expected == $value;
   }
 }
