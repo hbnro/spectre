64f5179210e330fb4b097e1c3e3e32ba5e0119d0
Alvaro Cabrera <aduran@expansion.com.mx>
2013-10-29 21:25:21 -0600

    Update specs

--- specs/spectre-spec.php
+++ specs/spectre-spec.php
@@ -16,39 +16,78 @@ describe('Spectre:', function () {
   });
 
 
-  describe('About php-spec matchers:', function () {
+  // TODO: do intensive testing!
+  describe('About matchers:', function () {
     $tests = array(
-      // TODO: improve this
-      'toContain' => array(array(1, 2, 3), 2),
-      'toBeLike' => array(1, '1'),
+      'toEqual' => array(1, '1'),
+      '!toEqual' => array(1, 2),
       'toBe' => array(1, 1),
-      'toEqual' => array(1, 1),
+      '!toBe' => array(1, '1'),
+      'toMatch' => array('bAz', '/A/'),
+      '!toMatch' => array('FoO', '/f/'),
       'toBeNull' => array(),
+      '!toBeNull' => array(1),
+      'toBeTruthy' => array('1'),
+      '!toBeTruthy' => array(),
+      'toBeFalsy' => array(),
+      '!toBeFalsy' => array('2'),
+      'toContain' => array('candy', 'andy'),
+      '!toContain' => array(array(1, 2, 3), 4),
+      'toBeLessThan' => array(2, 3),
+      '!toBeLessThan' => array(3, 2),
+      'toBeGreaterThan' => array(array(1, 2), array()),
+      '!toBeGreaterThan' => array(array(), array(2, 1)),
+      'toBeEmpty' => array(),
+      '!toBeEmpty' => array(array(1)),
       'toBeString' => array(''),
-      'toBeBoolean' => array(!0),
-      'toBeObject' => array(new \stdClass),
-      'toBeArray' => array(array()),
+      '!toBeString' => array(),
       'toBeInteger' => array(1),
-      'toBeCallable' => array(function () {}),
+      '!toBeInteger' => array('1'),
+      'toBeArray' => array(array()),
+      '!toBeArray' => array(),
+      'toBeInstanceOf' => array(new \stdClass, 'stdClass'),
+      '!toBeInstanceOf' => array(1, '1'),
+      'toBeLike' => array(2, '2'),
+      '!toBeLike' => array(2, 1),
+      'toBeBoolean' => array(!0),
+      '!toBeBoolean' => array(),
+      'toBeObject' => array(new \stdClass),
+      '!toBeObject' => array(),
+      'toBeCallable' => array('strlen'),
+      '!toBeCallable' => array(),
       'toBeDouble' => array(1.0),
-      'toBeFloat' => array(1.0),
-      'toBeLong' => array(1),
-      'toBeNumeric' => array('1.0'),
-      'toBeReal' => array(1.0),
-      'toBeScalar' => array('0'),
-      'toEndWith' => array('abc', 'c'),
-      'toStartWith' => array('def', 'd'),
-      'toMatch' => array('bAr', '/[A-Z]/'),
-      'toBeAnInstanceOf' => array(new \stdClass, 'stdClass'),
-      'toThrow' => array(function () { throw new \Exception('unknown'); }),
+      '!toBeDouble' => array('1.0'),
+      'toBeFloat' => array(2.0),
+      '!toBeFloat' => array('2.0'),
+      'toBeLong' => array(3),
+      '!toBeLong' => array('3'),
+      'toBeNumeric' => array('13.20'),
+      '!toBeNumeric' => array(),
+      'toBeReal' => array(4.0),
+      '!toBeReal' => array('4.0'),
+      'toBeScalar' => array(true),
+      '!toBeScalar' => array(),
+      'toEndWith' => array('foo', 'o'),
+      '!toEndWith' => array('bar', 'baz'),
+      'toStartWith' => array('abc', 'a'),
+      '!toStartWith' => array('def', 'f'),
+      'toThrow' => array(function () { throw new \Exception; }),
+      '!toThrow' => array(function () {}),
     );
 
 
     foreach ($tests as $fn => $args) {
       $subject = array_shift($args);
-      $callback = array(expect($subject), $fn);
 
-      it("testing $fn() matcher", function () use ($callback, $args) {
+      if (substr($fn, 0, 1) === '!') {
+        $callback = array(expect($subject)->not, substr($fn, 1));
+        $kall = 'NOT ' . substr($fn, 1);
+      } else {
+        $callback = array(expect($subject), $fn);
+        $kall = $fn;
+      }
+
+      it("testing $kall() matcher", function () use ($callback, $args) {
         call_user_func_array($callback, $args);
       });
     }
