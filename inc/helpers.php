c97309eb91f8b3db32e12e6ee255c20fe39915c2
Alvaro Cabrera <pateketrueke@gmail.com>
2013-12-12 04:10:10 -0600

    Compact code FTL

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,41 +1,33 @@
 <?php
 
-function expect($value)
-{
-  return \Spectre\Expect::that($value);
+if (!function_exists('expect')) {
+  function expect($value) { return \Spectre\Expect::that($value); }
 }
 
-function let($key, $value)
-{
-  \Spectre\Base::local($key, $value);
+if (!function_exists('let')) {
+  function let($key, $value) { \Spectre\Base::local($key, $value); }
 }
 
-function describe($desc, $cases)
-{
-  \Spectre\Base::add($desc, $cases);
+if (!function_exists('describe')) {
+  function describe($desc, $cases) { \Spectre\Base::add($desc, $cases); }
 }
 
-function it($desc, $test)
-{
-  \Spectre\Base::push($desc, $test);
+if (!function_exists('it')) {
+  function it($desc, $test) { \Spectre\Base::push($desc, $test); }
 }
 
-function before($block)
-{
-  \Spectre\Base::prepend($block);
+if (!function_exists('before')) {
+  function before($block) { \Spectre\Base::prepend($block); }
 }
 
-function beforeEach($block)
-{
-  \Spectre\Base::prepend($block, true);
+if (!function_exists('beforeEach')) {
+  function beforeEach($block) { \Spectre\Base::prepend($block, true); }
 }
 
-function after($block)
-{
-  \Spectre\Base::append($block);
+if (!function_exists('after')) {
+  function after($block) { \Spectre\Base::append($block); }
 }
 
-function afterEach($block)
-{
-  \Spectre\Base::append($block, true);
+if (!function_exists('afterEach')) {
+  function afterEach($block) { \Spectre\Base::append($block, true); }
 }
