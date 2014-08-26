bca9303d553bc84c487c92aa4b8c543e036f647b
Ek' Lamat <pateketrueke@gmail.com>
2014-08-26 12:51:11 +0000

    Fixed arguments on callback toPrint()

--- lib/Spectre/Matchers/ToPrint.php
+++ lib/Spectre/Matchers/ToPrint.php
@@ -7,8 +7,8 @@ class ToPrint extends Base
   public function execute($value)
   {
     ob_start();
-    $args = func_get_args();
-    echo is_callable($this->expected) ? call_user_func_array($this->expected, $args) : $this->expected;
+    $args = array_slice(func_get_args(), 1);
+    print_r(is_callable($this->expected) ? call_user_func_array($this->expected, $args) : $this->expected);
     $test = ob_get_clean();
 
     return $test === $value;
