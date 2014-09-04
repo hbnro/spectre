bfd4d4419ca2f73c8b1774c0fcb51d21218369ac
Ek' Lamat <pateketrueke@gmail.com>
2014-09-04 05:45:44 +0000

    White space

--- specs/context-spec.php
+++ specs/context-spec.php
@@ -1,25 +1,25 @@
 <?php
 
 describe('About context', function () {
-    let('global', 'VALUE');
+  let('global', 'VALUE');
 
-    it('should share context locally', function ($global) {
-        expect($global)->toBe('VALUE');
+  it('should share context locally', function ($global) {
+    expect($global)->toBe('VALUE');
+  });
+
+  describe('inner tests', function () {
+    let('local', 'TEST');
+
+    it('should share the upper context too', function ($global, $local) {
+      expect($global)->toBe('VALUE');
+      expect($local)->toBe('TEST');
     });
+  });
 
-    describe('inner tests', function () {
-        let('local', 'TEST');
-
-        it('should share the upper context too', function ($global, $local) {
-            expect($global)->toBe('VALUE');
-            expect($local)->toBe('TEST');
-        });
+  describe('sibling tests', function () {
+    it('should not share their siblings context', function ($global, $local) {
+      expect($global)->toBe('VALUE');
+      expect($local)->not->toBe('TEST');
     });
-
-    describe('sibling tests', function () {
-        it('should not share their siblings context', function ($global, $local) {
-            expect($global)->toBe('VALUE');
-            expect($local)->not->toBe('TEST');
-        });
-    });
+  });
 });
