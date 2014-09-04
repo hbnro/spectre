f91944cfc4c9177b2a78f54908c99f27ee5a7433
Ek' Lamat <pateketrueke@gmail.com>
2014-09-04 08:01:06 +0000

    Organize specs

--- specs/core-spec.php
+++ specs/core-spec.php
@@ -2,17 +2,48 @@
 
 // based on the jasmine test suite
 
-namespace My\Custom\Tests;
+describe('About let()', function () {
+  let('global', 'VALUE');
+  let('dummy', new \stdClass());
 
-class MyCustomMatcher extends \Spectre\Matchers\Base
-{
-  public function execute($value) {
-    return $this->expected === $value;
-  }
-}
+  it('should share context locally', function ($global) {
+    expect($global)->toBe('VALUE');
+  });
 
-\Spectre\Base::addMatcher('\\My\\Custom\\Tests\\MyCustomMatcher');
-\Spectre\Base::addMatcher('\\My\\Custom\\Tests\\MyCustomMatcher', 'toBeCustomValue');
+  describe('inner tests', function () {
+    let('local', 'TEST');
+
+    it('should share the upper context too', function ($global, $local) {
+      expect($global)->toBe('VALUE');
+      expect($local)->toBe('TEST');
+    });
+  });
+
+  describe('sibling tests', function () {
+    it('should not share their siblings context', function ($global, $local) {
+      expect($global)->toBe('VALUE');
+      expect($local)->not->toBe('TEST');
+    });
+  });
+
+  describe('callbacks inside tests', function ($dummy) {
+    it('should share context locally', function ($dummy) {
+      expect(function ($global, $local) {
+        echo $local ?: $global;
+      })->toPrint('VALUE');
+    });
+  });
+
+  describe('objects within inner tests', function ($dummy) {
+    before(function ($dummy) {
+      $dummy->foo = 'bar';
+    });
+
+    it('should share objects by reference', function ($dummy) {
+      expect($dummy->foo)->toBe('bar');
+    });
+  });
+});
 
 describe('About describe()', function () {
   let('a', true);
@@ -67,127 +98,12 @@ describe('About describe()', function () {
   });
 });
 
-describe('About expect()', function () {
-  it('receive a single value for test', function () {
-    expect(function () {
-      expect();
-    })->toWarn('Undefined variable: value');
-  });
-
-  describe('included matchers', function () {
-    let('a', '3');
-    let('b', 4);
-    let('c', 5);
-
-    it('toBe() compares with ===', function ($a, $b, $c) {
-      expect(($a * $a) + ($b * $b))->tobe($c * $c);
-      expect($a)->not->toBe(null);
-    });
-
-    describe('toEqual()', function () {
-      let('foo', array(
-        'a' => 12,
-        'b' => 34,
-      ));
-
-      let('bar', array(
-        'a' => 12,
-        'b' => 34,
-      ));
-
-      it('works for scalar variables', function ($a, $b) {
-        expect($a * $b)->toEqual(12);
-      });
-
-      it('should work for objects', function ($foo, $bar) {
-        expect($foo)->toEqual($bar);
-      });
-    });
-
-    it('toMatch() is for regular expressions', function () {
-      $message = 'foo bar baz';
-
-      expect($message)->toMatch('/bar/');
-      expect($message)->not->toMatch('/quux/');
-    });
-
-    it('toBeNull() compares against null', function ($x) {
-      expect($x)->toBeNull();
-      expect(null)->toBeNull();
-      expect('foo')->not->toBeNull();
-    });
-
-    it('toBeTruthy() is for boolean casting testing', function ($x) {
-      expect('foo')->toBeTruthy();
-      expect($x)->not->toBeTruthy();
-    });
-
-    it('toBeFalsy() is for boolean casting testing', function ($x) {
-      expect($x)->toBeFalsy();
-      expect('foo')->not->toBeFalsy();
-      expect(new \stdClass())->not->toBeFalsy();
-    });
-
-    describe('toContain()', function () {
-      it('works finding values on arrays', function () {
-        $x = array('foo', 'bar', 'baz');
-
-        expect($x)->toContain('bar');
-        expect($x)->not->toContain('quux');
-      });
-
-      it('should work with strings', function () {
-        $x = 'foo bar baz';
-
-        expect($x)->toContain('bar');
-        expect($x)->not->toContain('quux');
-      });
-    });
-
-    it('toBeLessThan() is for mathematical comparisons', function () {
-      $pi = 3.1415926;
-      $e = 2.78;
-
-      expect($e)->toBeLessThan($pi);
-      expect($pi)->not->toBeLessThan($e);
-    });
-
-    it('toBeGreaterThan() is for mathematical comparisons', function () {
-      $pi = 3.1415926;
-      $e = 2.78;
-
-      expect($pi)->toBeGreaterThan($e);
-      expect($e)->not->toBeGreaterThan($pi);
-    });
-
-    it('toWarn() is for testing if a function raises a warning', function () {
-      $foo = function () {
-        return 1 + 2;
-      };
-      $bar = function () {
-        return $a + 1;
-      };
-
-      expect($foo)->not->toWarn();
-      expect($bar)->toWarn(E_NOTICE);
-    });
-  });
-});
-
-
 describe('Pending specs', function () {
   xit("can be declared 'xit'", function () {
     expect(true)->toBe(false);
   });
 
   it("can be declared with 'it' but without a function");
-});
-
-describe('Custom matchers', function () {
-  it('should validate custom matchers', function () {
-    expect('foo')->toBeCustomValue('foo');
-    expect('bar')->MyCustomMatcher('bar');
-  });
 });
 
 describe('Isolation tests', function () {
---
+++ specs/testing-spec.php
@@ -0,0 +1,152 @@
+<?php
+
+// based on the jasmine test suite
+
+namespace My\Custom\Tests;
+
+class MyCustomMatcher extends \Spectre\Matchers\Base
+{
+  public function execute($value) {
+    return $this->expected === $value;
+  }
+}
+
+\Spectre\Base::addMatcher('\\My\\Custom\\Tests\\MyCustomMatcher');
+\Spectre\Base::addMatcher('\\My\\Custom\\Tests\\MyCustomMatcher', 'toBeCustomValue');
+
+describe('About expect()', function () {
+  it('receive a single value for test', function () {
+    expect(function () {
+      expect();
+    })->toWarn('Undefined variable: value');
+  });
+
+  describe('custom matchers', function () {
+    it('should validate custom matchers', function () {
+      expect('foo')->toBeCustomValue('foo');
+      expect('bar')->MyCustomMatcher('bar');
+    });
+  });
+
+  describe('included matchers', function () {
+    let('a', '3');
+    let('b', 4);
+    let('c', 5);
+
+    it('toBe() compares with ===', function ($a, $b, $c) {
+      expect(($a * $a) + ($b * $b))->tobe($c * $c);
+      expect($a)->not->toBe(null);
+    });
+
+    describe('toEqual()', function () {
+      let('foo', array(
+        'a' => 12,
+        'b' => 34,
+      ));
+
+      let('bar', array(
+        'a' => 12,
+        'b' => 34,
+      ));
+
+      it('works for scalar variables', function ($a, $b) {
+        expect($a * $b)->toEqual(12);
+      });
+
+      it('should work for objects', function ($foo, $bar) {
+        expect($foo)->toEqual($bar);
+      });
+    });
+
+    it('toMatch() is for regular expressions', function () {
+      $message = 'foo bar baz';
+
+      expect($message)->toMatch('/bar/');
+      expect($message)->not->toMatch('/quux/');
+    });
+
+    it('toBeNull() compares against null', function ($x) {
+      expect($x)->toBeNull();
+      expect(null)->toBeNull();
+      expect('foo')->not->toBeNull();
+    });
+
+    it('toBeTruthy() is for boolean casting testing', function ($x) {
+      expect('foo')->toBeTruthy();
+      expect($x)->not->toBeTruthy();
+    });
+
+    it('toBeFalsy() is for boolean casting testing', function ($x) {
+      expect($x)->toBeFalsy();
+      expect('foo')->not->toBeFalsy();
+      expect(new \stdClass())->not->toBeFalsy();
+    });
+
+    describe('toContain()', function () {
+      it('works finding values on arrays', function () {
+        $x = array('foo', 'bar', 'baz');
+
+        expect($x)->toContain('bar');
+        expect($x)->not->toContain('quux');
+      });
+
+      it('should work with strings', function () {
+        $x = 'foo bar baz';
+
+        expect($x)->toContain('bar');
+        expect($x)->not->toContain('quux');
+      });
+    });
+
+    it('toBeLessThan() is for mathematical comparisons', function () {
+      $pi = 3.1415926;
+      $e = 2.78;
+
+      expect($e)->toBeLessThan($pi);
+      expect($pi)->not->toBeLessThan($e);
+    });
+
+    it('toBeGreaterThan() is for mathematical comparisons', function () {
+      $pi = 3.1415926;
+      $e = 2.78;
+
+      expect($pi)->toBeGreaterThan($e);
+      expect($e)->not->toBeGreaterThan($pi);
+    });
+
+    it('toWarn() is for testing if a function raises a warning', function () {
+      $foo = function () {
+        return 1 + 2;
+      };
+      $bar = function () {
+        return $a + 1;
+      };
+
+      expect($foo)->not->toWarn();
+      expect($bar)->toWarn(E_NOTICE);
+    });
+
+    // TODO:
+    it('toBeEmpty()');
+    it('toBeString()');
+    it('toBeInteger()');
+    it('toBeArray()');
+    it('toBeInstanceOf()');
+    it('toBeLike()');
+    it('toBeBoolean()');
+    it('toBeObject()');
+    it('toBeCallable()');
+    it('toBeDouble()');
+    it('toBeFloat()');
+    it('toBeLong()');
+    it('toBeNumeric()');
+    it('toBeReal()');
+    it('toBeScalar()');
+    it('toEndWith()');
+    it('toStartWith()');
+    it('toThrow()');
+    it('toPrint()');
+    it('toHaveKey()');
+    it('toHaveLength()');
+  });
+});
