a61de729624362af1383d15afb5d30f45b25195a
Alvaro Cabrera <pateketrueke@gmail.com>
2014-09-05 17:28:31 -0500

    Fixed matchers for supporting objects

--- inc/matchers/to_contain.php
+++ inc/matchers/to_contain.php
@@ -4,6 +4,10 @@
     return false !== @array_search($value, $expected);
   }
 
+  if (is_object($expected)) {
+    return false !== @array_search($value, get_object_vars($expected));
+  }
+
   return is_string($value) && $value &&
          is_string($expected) && $expected &&
          (false !== strpos($expected, $value));
--- inc/matchers/to_have_key.php
+++ inc/matchers/to_have_key.php
@@ -1,4 +1,4 @@
 <?php return function ($expected, $value)
 {
-  return is_array($expected) && isset($expected[$value]);
+  return (is_array($expected) && isset($expected[$value])) || (is_object($expected) && isset($expected->$value));
 };
