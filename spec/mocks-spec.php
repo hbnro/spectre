5c1dfa1ba264e2752103b9226e1e839d34f5b954
Alvaro Cabrera <pateketrueke@gmail.com>
2016-01-31 01:36:45 -0600

    Specs for mocker

---
+++ spec/mocks-spec.php
@@ -0,0 +1,26 @@
+<?php
+
+class Foo {
+    public function bar() {
+        return $this->baz().'!';
+    }
+
+    public function baz() {
+        return 'buzz';
+    }
+}
+
+describe('Mocker', function () {
+    it('can mock instance methods', function () {
+        $stub = spy('Foo')
+            ->methods('baz')
+            ->callMethods(false)
+            ->getMock();
+
+        $stub->expects(exactly(1))
+            ->method('baz')
+            ->will(returnValue('foo'));
+
+        expect($stub->bar())->toEqual('foo!');
+    });
+});
